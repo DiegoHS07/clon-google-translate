c17dd7e9ae4c65e4dfc056e2f51e19557378b111
Diego Higuera <dhiguera09@gmail.com>
2023-10-16 13:04:38 -0500

    Create clipboard and speaker buttons

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,42 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { Container, Row, Col, Button, Stack } from 'react-bootstrap'
 import './App.css'
 import { useParams } from './hooks/useParams'
-import { AUTO_LANGUAGE } from "./constants"
-import { ArrowsIcon } from './components/Icons'
+import { AUTO_LANGUAGE, VOICE_FOR_LANGUAGES } from "./constants"
+import { ArrowsIcon, ClipboardIcon, SpeakerIcon } from './components/Icons'
 import { LanguageSelector } from './components/LanguageSelector'
 import { SectionType } from './types.d'
 import { TextArea } from './components/TextArea'
 import { useEffect } from 'react'
 import { translate } from '../src/services/translate'
+import { useDebounce } from './hooks/useDebounce'
 
 function App() {
   
   const { loading, fromLanguage, toLanguage, fromText, result, interchangeLanguages, setFromLanguage, setToLanguage, setFromText, setResult } = useParams()
+  
+  const debouncedFromText = useDebounce(fromText)
+
+  const handlerClipboard = () => {
+    navigator.clipboard.writeText(result).catch(() => { })
+  }
+
+  const handlerSpeaker = () => { 
+    const utterance = new SpeechSynthesisUtterance(result)
+    utterance.lang = VOICE_FOR_LANGUAGES[toLanguage]
+    utterance.rate = 0.9
+    speechSynthesis.speak(utterance)
+  }
+
   useEffect(() => {
-    if (fromText === '') return 
-    translate({ fromLanguage, toLanguage, text: fromText })
+    if (debouncedFromText === '') return 
+
+    translate({ fromLanguage, toLanguage, text: debouncedFromText })
       .then(result => {
         if (result == null) return
         setResult(result)
       })
       .catch(() => { setResult('Error')})
-  },[fromText])
+  },[debouncedFromText, fromLanguage, toLanguage])
 
   return (
     <Container fluid>
@@ -56,12 +72,30 @@ function App() {
               type={SectionType.To}
               value={toLanguage}
               onChange={setToLanguage} />
-            <TextArea
-              loading={loading}
-              type={SectionType.To}
-              value={result}
-              onChange={setResult}
-            />
+            <div style={{ position: "relative" }}>
+              <TextArea
+                loading={loading}
+                type={SectionType.To}
+                value={result}
+                onChange={setResult}
+              />
+              <div style={{ position: "absolute", right: 0, bottom: 0, display: "flex" }}>
+                <Button
+                  variant="link"
+                  onClick={handlerClipboard}
+                  title="Copiar Texto"
+                >
+                  <ClipboardIcon/>
+                </Button>
+                <Button
+                  variant="link"
+                  onClick={handlerSpeaker}
+                  title="Copiar Texto"
+                >
+                  <SpeakerIcon />
+                </Button>
+              </div>
+            </div>
           </Stack>
         </Col>
 
--- src/constants.ts
+++ src/constants.ts
@@ -3,6 +3,13 @@ export const SUPPORTED_LANGUAGES = {
   es: "Español",
   de: "Deutsch",
   it: "Italiano",
-};
+}
+
+export const VOICE_FOR_LANGUAGES = {
+  en: "en-GB",
+  es: "es-MX",
+  de: "de-DE",
+  it: "it-IT",
+}
 
 export const AUTO_LANGUAGE = 'auto'
